66260682343604e7ac30c3bd7de20c8444402b86
Raghavendra K R <j62@3ds.com>
2016-08-16 19:19:49 +0530

    some improvements

--- src/webcore/console/commands/BuildCommand.php
+++ src/webcore/console/commands/BuildCommand.php
@@ -10,13 +10,17 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use JMS\Composer\DependencyAnalyzer;
-use JMS\Composer\Graph\DependencyGraph;
 use JMS\Composer\Graph\PackageNode;
-use JMS\Composer\Graph\DependencyEdge;
+use NodejsPhpFallback\Less;
 
 class BuildCommand extends Command {
 	
 	private $builtPackages = array();
+	
+	/**
+	 * @var ConsoleLogger
+	 */
+	private $logger = null;
 	
 	protected function configure(){
 		$this->setName("build");
@@ -27,7 +31,7 @@ class BuildCommand extends Command {
 	
 	protected function execute(InputInterface $input,OutputInterface $output){
 		
-		$logger = new ConsoleLogger($output);
+		$this->logger = new ConsoleLogger($output);
 		
 		$reflectionClasss = new \ReflectionClass('Composer\Autoload\ClassLoader');
 		$classLoaderPath = $reflectionClasss->getFileName();
@@ -53,12 +57,12 @@ class BuildCommand extends Command {
 		
 		if(isset($composerJson["extra"]["web"]["build-dir"])){
 			if(!is_dir($composerJson["extra"]["web"]["build-dir"])){
-				$logger->warning("extra.web.build-dir is not a directory , so default build directory (dist) is used");
+				$this->logger->warning("extra.web.build-dir is not a directory , so default build directory (dist) is used");
 			}else{
 				$distDir = realpath($composerJson["extra"]["web"]["build-dir"]);
 			}
 		}else{
-			$logger->info("extra.web.build-dir is not set , so default build directory (dist) is used");
+			$this->logger->info("extra.web.build-dir is not set , so default build directory (dist) is used");
 		}
 		
 		$packageAnalyser = new DependencyAnalyzer();
@@ -74,7 +78,7 @@ class BuildCommand extends Command {
 		
 		$packageName = $packageNode->getName();
 		
-		if(in_array($packageName, $this->builtPackages) || $packageNode->isPhpExtension()){
+		if(in_array($packageName, $this->builtPackages) || $packageNode->isPhpExtension() || !$packageNode->hasAttribute('dir')){
 			return;
 		}
 		
@@ -86,12 +90,81 @@ class BuildCommand extends Command {
 			
 		}
 		
-		
-		echo $packageName."\n";
+		// Package directory
+		$pkgDirectory = $packageNode->getAttribute("dir");
 		$this->builtPackages[] = $packageName;
+		if(!file_exists($pkgDirectory.'/composer.json')){
+			$this->logger->warning("SKIPPING Build for $packageName : composer.json not found");
+			return;
+		}
 		
-		if($packageName == "php"){
-			print_r($packageNode);
+		$webSrcDirectory = $pkgDirectory."/websrc";
+		if(!is_dir($webSrcDirectory)){
+			$this->logger->info("SKIPPING Build for $packageName : no 'websrc' directory found");
+			return;
+		}
+		
+		// read the composer.json
+		$composerJsonData = json_decode(file_get_contents($pkgDirectory.'/composer.json'),true);
+		
+		if($composerJsonData == null){
+			$this->logger->warning("SKIPPING Build for $packageName : Invalid composer.json");
+			return;
+		}
+		
+		$pkgBuildConfig = null;
+		if(isset($composerJsonData["extra"]["web"])){
+			$pkgBuildConfig = $composerJsonData["extra"]["web"];
+		}
+		
+		$this->buildWebSource($webSrcDirectory, $pkgBuildConfig, $distDir, $packageName);
+		
+	}
+	
+	/**
+	 * 
+	 * @param string $webSrcDirectory
+	 * @param array  $config
+	 * @param string $distDir
+	 * @param string $packageName
+	 */
+	private function buildWebSource($webSrcDirectory,$config,$distDir,$packageName){
+		if(!isset($config)){
+			$this->logger->warning("SKIPPING Build for $packageName : Invalid composer.json");
+			return;
+		}
+		
+		if(is_array($config["compilers"])){
+			foreach ($config["compilers"] as $compilerName=>$compilerOptions){
+				switch (strtolower($compilerName)){
+					case "less":
+						if(is_array($compilerOptions)){
+							foreach ($compilerOptions as $destinationFile=>$sourceFiles){
+								if(is_string($sourceFiles)){
+									$sourceFiles = array($sourceFiles);
+								}
+
+								$destinationFile = $distDir.'/'.$destinationFile;
+								
+								if(file_exists($destinationFile)){
+									$this->logger->warning("$packageName is overwriting existing file : $destinationFile");
+								}
+								$this->ensureFileDir($destinationFile);
+								foreach ($sourceFiles as $sourceFile){
+									$lessCompiler = new Less($webSrcDirectory.'/'.$sourceFile);
+									file_put_contents($destinationFile, $lessCompiler->getResult(),FILE_APPEND);
+								}
+								
+							}
+						}
+				}
+			}
+		}
+	}
+	
+	private function ensureFileDir($path){
+		if(!is_dir(dirname($path))){
+			mkdir(dirname($path),null,true);
 		}
 	}
 	
